d44a364038ed9f56acae2e482bca84dd256fbeab
DanielBenjaminLopez <benja248myn@gmail.com>
2022-08-22 20:39:05 -0300

    Agrega apps a installed_apps

--- homebanking/homebanking/settings.py
+++ homebanking/homebanking/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'clientes',
+    'cuentas',
+    'login',
+    'prestamos',
+    'tarjetas',
 ]
 
 MIDDLEWARE = [
