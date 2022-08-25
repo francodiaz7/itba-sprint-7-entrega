f96d625a81456e7c892d733c9146d5ad7e8a3e2d
DanielBenjaminLopez <benja248myn@gmail.com>
2022-08-25 20:04:12 -0300

    Soluciona un par de errores de cliente

--- homebanking/clientes/apps.py
+++ homebanking/clientes/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
-class ClientesConfig(AppConfig):
+class ClienteConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'clientes'
     verbose_name = 'Cliente'
